815d618b290f51c136bdbc9171405d2d7962ba8b
samia-ctrl <samiauddin99@gmail.com>
2024-03-28 18:09:38 +0000

    began populating services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,10 +1,32 @@
 import NavBar from "../components/NavBar";
-
+import Footer from "../components/Footer";
+import "../Services.css"
 export default function Services() {
   return (
     <>
       <NavBar showLogo={true} />
-      <h2 className="heading">Services</h2>
+      <h2 className="heading">
+        <em>Services</em> we offer
+      </h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-4  service-heading">Tenant Placement</div>
+          <div className="col-4  service-heading">
+            Property Maintanance
+          </div>
+          <div className="col-4  service-heading">
+            Professional Cleaning
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-4  service-heading">No Rental Voids</div>
+          <div className="col-4  service-heading">No Rental Arrears</div>
+          <div className="col-4  service-heading">
+            Tenant Damage Cover
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
