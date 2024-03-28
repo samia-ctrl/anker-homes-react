77f98dd980261d1e8378371415e78e48b0926f57
samia-ctrl <samiauddin99@gmail.com>
2024-03-28 21:50:26 +0000

    services page populated

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -8,21 +8,81 @@ export default function Services() {
       <h2 className="heading">
         <em>Services</em> we offer
       </h2>
-      <div className="container">
+      <div className="container ">
         <div className="row">
-          <div className="col-4  service-heading">Tenant Placement</div>
-          <div className="col-4  service-heading">
-            Property Maintanance
+          <div className="col-md-4  ">
+            <div className="service-heading">
+              {" "}
+              <em>Tenant Placement</em>
+              <div className="body services-para">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.{" "}
+              </div>
+            </div>
           </div>
-          <div className="col-4  service-heading">
-            Professional Cleaning
+          <div className="col-md-4  ">
+            <div className="service-heading">
+              {" "}
+              <em> Property Maintanance</em>
+              <div className="body services-para">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.{" "}
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4  ">
+            <div className="service-heading">
+              {" "}
+              <em> Professional Cleaning</em>
+              <div className="body services-para">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.{" "}
+              </div>
+            </div>
           </div>
         </div>
         <div className="row">
-          <div className="col-4  service-heading">No Rental Voids</div>
-          <div className="col-4  service-heading">No Rental Arrears</div>
-          <div className="col-4  service-heading">
-            Tenant Damage Cover
+          <div className="col-md-4 ">
+            <div className="service-heading">
+              {" "}
+              <em> No Rental Voids</em>
+              <div className="body services-para">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.{" "}
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4  ">
+            <div className="service-heading">
+              {" "}
+              <em> No Rental Arrears</em>
+              <div className="body services-para">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.{" "}
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4  ">
+            <div className="service-heading">
+              {" "}
+              <em>Tenant Damage Cover</em>
+              <div className="body services-para">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.{" "}
+              </div>
+            </div>
           </div>
         </div>
       </div>
